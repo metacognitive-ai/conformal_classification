48445e30288a51b48e121304d8d4f059288c0108
gungore <gungorerdi@gmail.com>
2022-08-09 14:39:36 +0300

    change_example

--- example.py
+++ example.py
@@ -1,59 +1,103 @@
 import argparse
+import sys
+from typing import Any
+
+from OpenSSL import SSL
+import fasttext
 import torch
-import torchvision
-import torchvision.transforms as transforms
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import time
-from utils import * 
+from utils import *
+from torch.utils.data import DataLoader
 from conformal import ConformalModel
 import torch.backends.cudnn as cudnn
 import random
+import torchtext
+from torchtext.vocab import FastText
 
-parser = argparse.ArgumentParser(description='Conformalize Torchvision Model on Imagenet')
-parser.add_argument('data', metavar='IMAGENETVALDIR', help='path to Imagenet Val')
-parser.add_argument('--batch_size', metavar='BSZ', help='batch size', default=128)
-parser.add_argument('--num_workers', metavar='NW', help='number of workers', default=0)
-parser.add_argument('--num_calib', metavar='NCALIB', help='number of calibration points', default=10000)
+
+parser = argparse.ArgumentParser(description='Conformalize Torch-FT Model')
+#parser.add_argument('data', metavar='IMAGENETVALDIR', help='path to Imagenet Val')
+#parser.add_argument('--batch_size', metavar='BSZ', help='batch size', default=128)
+#parser.add_argument('--num_workers', metavar='NW', help='number of workers', default=0)
+#parser.add_argument('--num_calib', metavar='NCALIB', help='number of calibration points', default=10000)
 parser.add_argument('--seed', metavar='SEED', help='random seed', default=0)
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    ### Fix randomness 
+    ### Fix randomness
     np.random.seed(seed=args.seed)
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
     random.seed(args.seed)
 
-    # Transform as in https://github.com/pytorch/examples/blob/42e5b996718797e45c46a25c55b031e6768f8440/imagenet/main.py#L92 
-    transform = transforms.Compose([
-                    transforms.Resize(256),
-                    transforms.CenterCrop(224),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std= [0.229, 0.224, 0.225])
-                ])
 
-    # Get the conformal calibration dataset
-    imagenet_calib_data, imagenet_val_data = torch.utils.data.random_split(torchvision.datasets.ImageFolder(args.data, transform), [args.num_calib,50000-args.num_calib])
+    #creating field
+    TEXT = torchtext.legacy.data.Field(lower=True, include_lengths=True, fix_length=150)
+    LABEL = torchtext.legacy.data.Field(sequential=True, use_vocab=False)
 
-    # Initialize loaders 
-    calib_loader = torch.utils.data.DataLoader(imagenet_calib_data, batch_size=args.batch_size, shuffle=True, pin_memory=True)
-    val_loader = torch.utils.data.DataLoader(imagenet_val_data, batch_size=args.batch_size, shuffle=True, pin_memory=True)
+    INDEX = torchtext.legacy.data.Field(sequential=False)
 
+    id_label = 'id'
+    text_label = 'text'
+    label = 'label'
+
+    train_fields=[
+        #(index, INDEX),
+        (text_label, TEXT),
+        (label, LABEL)]
+
+    train = torchtext.legacy.data.TabularDataset(
+        path='model_data/train.csv', format='csv', skip_header=True,
+        fields=train_fields)
+
+    test_fields=[
+        #(id_label, INDEX),
+        (text_label, TEXT),
+        (label, LABEL)
+    ]
+    test = torchtext.legacy.data.TabularDataset(
+        path='model_data/test.csv', format='csv', skip_header=True,
+        fields=test_fields)
+
+
+    '''
+    vectors = FastText('tr')
+
+    max_size = 30000
+    TEXT.build_vocab(train, test, vectors=vectors, max_size=max_size)
+    INDEX.build_vocab(test)
+    ntokens = len(TEXT.vocab)
+    print (ntokens)
+    '''
+    calib_loader= DataLoader(train, batch_size=16, shuffle=True)
+    val_loader = DataLoader(test, batch_size=16, shuffle=True)
+
+    '''
+    #iterator for training Neural network models
+    train_iterator = torchtext.legacy.data.BucketIterator(calib_loader, batch_size=32,
+                                                          sort_key=lambda x: len(x.text),
+                                                          sort_within_batch=True, repeat=False)
+    test_iterator = torchtext.legacy.data.BucketIterator(val_loader, batch_size=32,
+                                                         sort_key=lambda x: len(x.text),
+                                                         sort_within_batch=True, train=False, repeat=False)
+    '''
+
+    # Initialize loaders
     cudnn.benchmark = True
 
-    # Get the model 
-    model = torchvision.models.resnet152(pretrained=True,progress=True).cuda()
-    model = torch.nn.DataParallel(model) 
+    # Get the model
+    model = fasttext.load_model("model_data/61d84d0d632e1d67f87de8dd_TrainingIntent.bin")
+    model = torch.nn.DataParallel(model)
     model.eval()
 
     # optimize for 'size' or 'adaptiveness'
     lamda_criterion = 'size'
     # allow sets of size zero
-    allow_zero_sets = False 
+    allow_zero_sets = False
     # use the randomized version of conformal
-    randomized = True 
+    randomized = True
 
     # Conformalize model
     model = ConformalModel(model, calib_loader, alpha=0.1, lamda=0, randomized=randomized, allow_zero_sets=allow_zero_sets)
